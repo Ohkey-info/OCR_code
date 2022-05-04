ccec07f68615a33bd3f7dffc4a58dd0362458bf8
jaechansong <jachan9824@gmail.com>
2022-05-04 09:39:27 +0900

    +capture code

--- test_py.py
+++ test_py.py
@@ -5,7 +5,7 @@ import json
 
 api_url = 'https://1mte94hdpr.apigw.ntruss.com/custom/v1/15641/df7e3d874c4ebcd0839255e7bb5ecec3d3dc754994d59c437e615e4934d6fdcd/general'
 secret_key = 'UXdxaFVvdVRrSWRVQnRtUFhZVWZpUWZObGxnb05lcHA='
-image_url = 'https://capston-dgu.s3.ap-northeast-2.amazonaws.com/image.png'
+image_url = '파일 S3 URL'
 output_file = 'OCR.json'
 
 request_json = {
@@ -32,7 +32,7 @@ response = requests.request("POST", api_url, headers=headers, data = payload)
 
 #print(response.text)
 
-res = json.loads(response.text.encode('utf8'))
+res = json.loads(response.text)
 print(res)
 
 with open(output_file, 'w', encoding='utf-8') as outfile:
